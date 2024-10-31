268262e8252a56c9a69bcb73f37b188117cee1a7
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-10-31 10:07:12 -0300

    fixing a bug css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,10 @@
-"use client";
 
-import Search from "../components/Search/search";
 
 export default function Home() {
-  const fetch2 = async () => {
-    try {
-      const res = await fetch("./api/genresMovies");
-
-      // Check if the request was successful
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
-      }
-
-      // Parse JSON data from the response
-      const data = await res.json();
-      console.log(data); // Log the actual data from the response
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  };
-
-  fetch2();
-  // Empty
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Search />
+    <div className=''>
+      home
     </div>
   );
 }
